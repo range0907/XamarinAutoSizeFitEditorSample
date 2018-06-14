64a1743b86590f032af80fdd1749acc41d27313d
range0907 <falconguy.8810@gmail.com>
2018-06-14 17:16:43 +0900

    新しいリポジトリにしようかな・・・

--- AutSizeFitEditor/AutSizeFitEditor.Android/MainActivity.cs
+++ AutSizeFitEditor/AutSizeFitEditor.Android/MainActivity.cs
@@ -9,21 +9,21 @@ namespace AutSizeFitEditor.Droid
     [Activity(Label = "AutSizeFitEditor", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
-
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            base.OnCreate(savedInstanceState);
+            
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App(new AndroidInitializer()));
         }
     }
 
     public class AndroidInitializer : IPlatformInitializer
     {
-        public void RegisterTypes(IContainerRegistry container)
+        public void RegisterTypes(IContainerRegistry containerRegistry)
         {
             // Register any platform specific implementations
         }
